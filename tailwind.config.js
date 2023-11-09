8342fdd5eeb60103335fc0285b575e10793ea1c7
Ernesto1207 <ernestosolanoballena@hotmail.com>
2023-11-09 05:21:42 -0500

    preview

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('../public/hero.jpg')",
+        'card-image': "url('../public/img-1.webp')",
+        'card-two': "url('../public/img-2.webp')",
+        'card-tree':"url('../public/img-3.webp')",
+      },
+      brightness: {
+        25: '.25',
+        175: '1.75',
+      },
+      fontFamily:{
+        Merriweather: ['Merriweather', 'mono'],
+      },
+    },
   },
   plugins: [],
 }
-
